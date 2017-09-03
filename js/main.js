7c3452eaf770c617c3e8a92948897565f142a553
johnytiago <joaosao10@gmail.com>
2017-09-03 16:37:36 +0100

    Added intro, testimonials, team, classes and contact form

--- js/main.js
+++ js/main.js
@@ -1,48 +1,33 @@
 $(document).ready(function(){
-  $(".secure-left-hover").hover(
-    function handleIn(){
-      $("#left-drawer").css('left', 0);
-    },
-    function handleOut(){
-      $("#left-drawer").css('left', "-50%");
-    },
-  );
-  $(".secure-right-hover").hover(
-    function handleIn(){
-      $("#right-drawer").css('right', 0);
-    },
-    function handleOut(){
-      $("#right-drawer").css('right', "-50%");
-    },
-  );
-  $("#portfolio-contant-active").mixItUp();
-  $("#testimonial-slider").owlCarousel({
-    paginationSpeed : 500,      
-    singleItem:true,
-    autoPlay: 3000,
+  $("#intro-slider").owlCarousel({
+    items: 1,
+    singleItem: true,
+    loop: true,
+    margin:10,
+    pagination: true,
+    mouseDrag: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 4000
   });
-  $("#pricing-slider").owlCarousel({
-    singleItem:true,
-    autoPlay: false,
-    loop:false,
-  });
-  $("#works-logo").owlCarousel({
-    autoPlay: 3000,
-    items : 5,
-    itemsDesktop : [1199,5],
-    itemsDesktopSmall : [979,5],
-  });
-  //// google map
-  //var map;
-  //function initMap() {
-  //map = new google.maps.Map(document.getElementById('map'), {
-  //center: {lat: 38.707113, lng: -9.1881},
-  //zoom: 8
-  //});
-  //}
-  // Counter
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
+  $("#testimonials-slider").owlCarousel({
+    items: 1,
+    singleItem: true,
+    loop: true,
+    margin:10,
+    pagination: true,
+    mouseDrag: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 5000
   });
 });
+
+function sendmail() {
+  var subject = $("#emailSubject").val()
+  var body = $("#emailBody").val()
+  var link = "mailto:info@clubejiujitsulisboa.com"
+  + "?subject=" + subject
+  + "&body=" + body;
+  window.location.href = link;
+}
